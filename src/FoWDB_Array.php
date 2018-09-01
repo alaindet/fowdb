fd3dd3f3da2372052a952f06c1bc0389b1303464
Alain D'Ettorre <alain.det@gmail.com>
2018-09-01 22:12:50 +0200

    Fixed custom array union function

--- src/FoWDB_Array.php
+++ src/FoWDB_Array.php
@@ -13,14 +13,6 @@ class FoWDB_Array
      */
     public static function union($arr1, $arr2)
     {
-        // Initialize output
-        $o = [];
-        // Merge arrays
-        $arr = array_merge($arr1, $arr2);
-        // Loop on merged
-        // (Duplicates just set their *single* element multiple times!)
-        foreach($arr as $val) { $o[$val] = true; }
-        // Return unique values
-        return array_keys($o); 
+        return array_unique(array_merge($arr1, $arr2));
     }
 }
