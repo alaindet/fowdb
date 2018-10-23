6531f222afa7cc4c24cd8498f12dba1730a7b45c
Alain D'Ettorre <alain.det@gmail.com>
2018-10-23 19:20:38 +0200

    Add Login Controller and Redirect (to be used)

---
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Base\Controller;
+use App\Http\Request\Input;
+
+class LoginController extends Controller
+{
+    public function loginForm()
+    {
+
+    }
+
+    public function login(Input $input)
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
---
+++ src/app/Http/Response/Redirect.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Response;
+
+class Redirect
+{
+    public static function url(string $url = ''): string
+    {
+        return config('');
+    }
+
+    public static function to()
+    {
+
+    }
+}
