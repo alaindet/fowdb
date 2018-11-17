15574dacde6a0937dd5c51dbf79bf7bbd8d9389e
Alain D'Ettorre <alain.det@gmail.com>
2018-11-17 18:57:38 +0100

    Add Model::byId method

--- src/app/Base/Model.php
+++ src/app/Base/Model.php
@@ -7,7 +7,7 @@ use App\Exceptions\ModelException;
 
 abstract class Model extends Base
 {
-    public function all(array $select = null)
+    public function all(array $select = null): array
     {
         // ERROR: Missing table name
         if (!isset($this->table)) {
@@ -16,6 +16,33 @@ abstract class Model extends Base
 
         $fields = isset($select) ? implode(',', $select) : '*';
         
-        return database_old()->get("SELECT {$fields} FROM {$this->table}");
+        return database()
+            ->select(
+                statement('select')
+                    ->select($fields)
+                    ->from($this->table)
+            )
+            ->get();
+    }
+
+    public function byId($id, array $select = null): array
+    {
+        // ERROR: Missing table name
+        if (!isset($this->table)) {
+            throw new ModelException('Missing table name for model');
+        }
+
+        $fields = isset($select) ? implode(',', $select) : '*';
+
+        return database()
+            ->select(
+                statement('select')
+                    ->select($fields)
+                    ->from($this->table)
+                    ->where('id = :id')
+                    ->limit(1)
+            )
+            ->bind([':id' => intval($id)])
+            ->first();
     }
 }
