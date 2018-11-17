2f77112ababbeba42b3f248f6a3ffcfaeebc293d
Alain D'Ettorre <alain.det@gmail.com>
2018-11-17 11:10:29 +0100

    Add getClusterById to CardSet model

--- src/app/Models/CardSet.php
+++ src/app/Models/CardSet.php
@@ -7,4 +7,25 @@ use App\Base\Model;
 class CardSet extends Model
 {
     public $table = 'sets';
+
+    public const TABLE = 'sets';
+
+    public function getClusterById(string $setId): int
+    {
+        $data = database()
+            ->select(
+                statement('select')
+                    ->select('clusters_id')
+                    ->from(self::TABLE)
+                    ->where('id = :setid')
+                    ->limit(1)
+            )
+            ->from(self::TABLE)
+            ->bind([
+                ':setid' => $setId
+            ])
+            ->first();
+
+        return (int) $data['clusters_id'];
+    }
 }
