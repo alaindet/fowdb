1df513a9d2bc0122fadeaf42aa7c93647202972b
Alain D'Ettorre <alain.det@gmail.com>
2018-12-07 15:30:49 +0100

    Remove old CR manager

--- old/admin/cr/main.html.php
+++
@@ -1,8 +0,0 @@
-<div class="page-header">
-	<h1>
-		Comprehensive Rules
-		<small>Admin panel</small>
-	</h1>
-</div>
-<?php include __DIR__ . '/form.html.php'; ?>
-<?php include __DIR__ . '/list.html.php'; ?>
