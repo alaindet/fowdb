cac50ae7378eccf146dfe00daef1c3881425186c
Alain D'Ettorre <alain.det@gmail.com>
2018-12-07 21:59:50 +0100

    Add nextAvailableId method

--- src/app/Models/GameSet.php
+++ src/app/Models/GameSet.php
@@ -7,4 +7,11 @@ use App\Base\Model;
 class GameSet extends Model
 {
     public $table = 'game_sets';
+
+    public static function nextAvailableId(): int
+    {
+        $item = database()->rawSelect('SELECT max(id) max FROm game_sets');
+
+        return intval($item[0]['max']) + 1;
+    }
 }
