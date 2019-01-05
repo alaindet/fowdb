23b09b7b339b4ff8c1ccfca67581193e9fc8fd9e
Alain D'Ettorre <alain.det@gmail.com>
2019-01-06 00:26:01 +0100

    Add form/button-checkboxes component

---
+++ src/resources/views/components/form/button-checkboxes.tpl.php
@@ -0,0 +1,67 @@
+<?php
+
+// VARIABLES
+// name (optional)
+// items
+// css (optional)
+//   container
+//   button
+
+// NOTE
+// If input name is not given, each item has a different input name so that
+// $items = [ input_name => label, ... ]
+// instead of default behavior
+// $items = [ value => label, ... ]
+
+echo log_html([
+  'name' => $name,
+]);
+
+$containerCss = '';
+$buttonCss = '';
+
+if (isset($css)) {
+  if (isset($css['container'])) {
+    $containerCss = ' ' . implode(' ', $css['container']);
+  }
+  if (isset($css['button'])) {
+    $buttonCss = ' ' . implode(' ', $css['button']);
+  }
+}
+
+?>
+<div class="btg-group<?=$containerCss?>" data-toggle="buttons">
+  <?php if (isset($name)): ?>
+
+    <?php
+      // ITEMS: value => label
+      foreach ($items as $value => $label):
+    ?>
+      <label class="btn<?=$buttonCss?>">
+        <input
+          type="checkbox"
+          name="<?=$name?>[]"
+          value="<?=$value?>"
+        >
+        <span class="pointer"><?=$label?></span>
+      </label>
+    <?php endforeach; ?>
+
+  <?php else: ?>
+
+    <?php
+      // ITEMS: input name => label
+      foreach ($items as $inputName => $label):
+    ?>
+      <label class="btn<?=$buttonCss?>">
+        <input
+          type="checkbox"
+          name="<?=$inputName?>"
+          value="1"
+        >
+        <span class="pointer"><?=$label?></span>
+      </label>
+    <?php endforeach; ?>
+
+  <?php endif; ?>
+</div>
