e221fe8f7472659a8f0b6e8cd23d43b6976171b7
Alain D'Ettorre <alain.det@gmail.com>
2019-01-30 21:06:28 +0100

    Rename public/cards/search page script in public/cards/search-results

---
+++ src/resources/assets/js/pages/public/cards/search-results/search-results.js
@@ -0,0 +1,7 @@
+(function () {
+  // @partial: public/cards/search-results/partials/state
+  // @partial: public/cards/search-results/partials/controller
+  // @partial: public/cards/search-results/partials/model
+  // @partial: public/cards/search-results/partials/view
+  // @partial: public/cards/search-results/partials/bootstrap
+})();
--- src/resources/assets/js/pages/public/cards/search/search.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  // @partial: public/cards/search/partials/state
-  // @partial: public/cards/search/partials/controller
-  // @partial: public/cards/search/partials/model
-  // @partial: public/cards/search/partials/view
-  // @partial: public/cards/search/partials/bootstrap
-})();
