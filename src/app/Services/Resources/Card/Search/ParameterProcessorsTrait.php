a5369e403bbb65cdfc8c5b15cc70270fbb4c304c
Alain D'Ettorre <alain.det@gmail.com>
2019-01-30 01:09:23 +0100

    Add complete and new cards search service
    App\Services\Resources\Card\Search\Search (just "Search" here) is the
    main class extending App\Base\Search\Search. It does this
    - Define all parameter processor functions
    - Define parameter aliases for old parameters copy-pasted on the Web
    - Set the default table and fields for the SelectSqlStatement instance
    - Instantiate the Lookup object to retrieve lookup data
    - Import two traits ParameterProcessorsTrait and PostProcessingTrait
    
    ParameterProcessorsTrait has simply a method for every parameter. If
    the parameter alone maps to a search filter, the SelectSqlStatement
    instance is directly manipulated from its processig method. If the
    parameter is part of a complex search filter (which needs more than 1
    parameter to be set), its processing method just stores its value in a
    shared $state property for later use
    
    PostProcessingTrait defines the standard afterProcessing method, as
    defined into App\Base\Search\SearchInterface, and processes all
    "complex" search filters using $state, aka multiple parameters at once.
    
    Examples of "complex" search filters include the cards attribute (you
    could click "Only Multi-Attribute" or "Must contain just selected"
    flag) and the search bar text.

---
+++ src/app/Services/Resources/Card/Search/ParameterProcessorsTrait.php
@@ -0,0 +1,428 @@
+<?php
+
+namespace App\Services\Resources\Card\Search;
+
+use App\Base\Search\SearchInterface;
+use App\Models\CardType;
+
+/**
+ * From App\Services\Resources\Card\Search\Search
+ * ==============================================
+ * protected $statement;
+ * protected $bind;
+ * protected $state;
+ * 
+ * List of all possible state variables
+ * ====================================
+ * atk
+ * atk-operator
+ * attributes
+ * attributes-only-multi
+ * attributes-only-single
+ * attributes-selected
+ * cost
+ * cost-x
+ * def
+ * def-operator
+ * query
+ * query-exact
+ * query-fields
+ * sort
+ * sort-direction
+ * types
+ * types-selected
+ */
+trait ParameterProcessorsTrait
+{
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: query
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processQueryParameter(string $value): void
+    {
+        $this->state['query'] = $value;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: query-exact
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processPartialMatchParameter(string $value): void
+    {
+        $this->state['query-exact'] = false;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: query-fields
+     *
+     * @param array $value
+     * @return void
+     */
+    protected function processInFieldsParameter(string $value): void
+    {
+        $this->state['query-fields'] = $value;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: sort
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processSortParameter(string $value): void
+    {
+        if ($value === 'no') return;
+
+        $this->state['sort'] = $value;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: sort-direction
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processSortDirectionParameter(string $value): void
+    {
+        $this->state['sort-direction'] = $value;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: attributes
+     *
+     * @param array $values
+     * @return void
+     */
+    protected function processAttributeParameter(array $values): void
+    {
+        $this->state['attributes'] = $values;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: attributes-selected
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processSelectedAttributeParameter(string $value): void
+    {
+        $this->state['attributes-selected'] = true;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: attributes-only-single
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processSingleAttributeParameter(string $value): void
+    {
+        $this->state['attributes-only-single'] = true;
+    }
+
+    /**
+     * Sets a state variable for later use (multi-input processor)
+     * 
+     * State: attributes-only-multi
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processMultiAttributeParameter(string $value): void
+    {
+        $this->state['attributes-only-multi'] = true;
+    }
+
+    /**
+     * Sets a state variable instead of adding a filter
+     * 
+     * State: atk-operator
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processAtkOperatorParameter(string $value): void
+    {
+        $this->state['atk-operator'] = $value;
+    }
+
+    /**
+     * Sets a state variable instead of adding a filter
+     * 
+     * State: atk
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processAtkValueParameter(string $value): void
+    {
+        $this->state['atk'] = $value;
+    }
+
+    /**
+     * Sets a state variable instead of adding a filter
+     * 
+     * State: def-operator
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processDefOperatorParameter(string $value): void
+    {
+        $this->state['def-operator'] = $value;
+    }
+
+    /**
+     * Sets a state variable instead of adding a filter
+     * 
+     * State: def
+     *
+     * @param string $value
+     * @return void
+     */
+    protected function processDefValueParameter(string $value): void
+    {
+        $this->state['def'] = $value;
+    }
+
+    /**
+     * Sets a state variable instead of adding a filter
+     * 
+     * State: types
+     *
+     * @param string $values
+     * @return void
+     */
+    protected function processTypeParameter(array $values): void
+    {
+        $this->state['types'] = $values;
+    }
+
+    /**
+     * Sets a state variable instead of adding a filter
+     * 
+     * State: types-selected
+     *
+     * @param string $values
+     * @return void
+     */
+    protected function processTypeSelectedParameter(array $values): void
+    {
+        $this->state['types-selected'] = true;
+    }
+
+    protected function processExcludeParameter(array $values): void
+    {
+        foreach ($values as $toExclude) {
+            switch ($toExclude) {
+                case 'spoilers':
+                    $list = implode(',', $this->lookup->get('spoilers.ids'));
+                    $this->statement->where("NOT(sets_id IN({$list}))");
+                    break;
+                case 'basics':
+                    $this->statement->where('narp <> 0');
+                    break;
+                case 'alternates':
+                    $this->statement->where('narp <> 1');
+                    break;
+                case 'reprints':
+                    $this->statement->where('narp <> 2');
+                    break;
+            }
+        }
+    }
+
+    protected function processFormatParameter(array $values): void
+    {
+        $clusters = [];
+        $map = $this->lookup->get('formats.code2clusters');
+        for ($i = 0, $ii = count($values); $i < $ii; $i++) {
+            $formatClusters = $map[$values[$i]] ?? [];
+            $clusters = array_merge($clusters, $formatClusters);
+        }
+        $clusters = implode(',', array_unique($clusters));
+
+        $this->statement->where("clusters_id IN ({$clusters})");
+    }
+
+    protected function processBackSideParameter(array $values): void
+    {
+        $where = [];
+        $baseKey = ':backside';
+        $keyCounter = 1;
+        $map = $this->lookup->get('backsides.code2id');
+
+        for ($i = 0, $ii = count($values); $i < $ii; $i++) {
+            $temp = $map[$values[$i]] ?? null;
+            if (!isset($temp)) continue;
+            $key = $baseKey . $keyCounter++;
+            $this->bind[$key] = $temp;
+            $where[] = "back_side = {$key}";
+        }
+
+        $this->statement->where($where, 'OR');
+    }
+
+    protected function processDivinityParameter(array $values): void
+    {
+        $where = [];
+        $baseKey = ':divinity';
+        $keyCounter = 1;
+
+        for ($i = 0, $ii = count($values); $i < $ii; $i++) {
+            $key = $baseKey . $keyCounter++;
+            $this->bind[$key] = $values[$i];
+            $where[] = "divinity = {$key}";
+        }
+
+        $this->statement->where($where, 'OR');
+    }
+
+    /**
+     * Allows both array and string as input
+     *
+     * @param string|string[] $value
+     * @return void
+     */
+    protected function processSetParameter($value): void
+    {
+        $map = $this->lookup->get('sets.code2id');
+        
+        // Single set
+        if (is_string($value)) {
+            if ($value === 'no' || $value === '0') return;
+            $this->statement->where('sets_id = :setid');
+            $this->bind[':setid'] = $map[$value];
+            return;
+        }
+
+        // Multiple sets
+        for ($i = 0, $ii = count($value); $i < $ii; $i++) {
+            $validValue = $map[$value[$i]] ?? null;
+            if (!isset($validValue)) unset($value[$i]);
+            else $value[$i] = $validValue;
+        }
+
+        $values = implode(',', $value);
+        $this->statement->where("sets_id IN ({$values})");
+    }
+
+    /**
+     * Allows both array and string as input
+     *
+     * @param string|string[] $value
+     * @return void
+     */
+    protected function processClusterParameter($value): void
+    {
+        $map = $this->lookup->get('clusters.code2id');
+        
+        // Single cluster
+        if (is_string($value)) {
+            if ($value === 'no' || $value === '0') return;
+            $this->statement->where('clusters_id = :clusterid');
+            $this->bind[':clusterid'] = $map[$value];
+            return;
+        }
+
+        // Multiple clusters
+        for ($i = 0, $ii = count($value); $i < $ii; $i++) {
+            $validValue = $map[$value[$i]] ?? null;
+            if (!isset($validValue)) unset($value[$i]);
+            else $value[$i] = $validValue;
+        }
+        $values = implode(',', $value);
+        $this->statement->where("clusters_id IN ({$values})");
+    }
+
+    protected function processCostParameter(array $values): void
+    {
+        $where = [];
+        $baseKey = ':cost';
+        $keyCounter = 1;
+
+        for ($i = 0, $ii = count($values); $i < $ii; $i++) {
+            $key = $baseKey . $keyCounter++;
+            $this->bind[$key] = $values[$i];
+            $where[] = "total_cost = {$key}";
+        }
+
+        $this->statement->where($where, 'OR');
+    }
+
+    protected function processFreeCostParameter(array $values): void
+    {
+        $where = [];
+        $baseKey = ':freecost';
+        $keyCounter = 1;
+
+        for ($i = 0, $ii = count($values); $i < $ii; $i++) {
+            $key = $baseKey . $keyCounter++;
+            $this->bind[$key] = $values[$i];
+            $where[] = "free_cost = {$key}";
+        }
+
+        $this->statement->where($where, 'OR');
+    }
+
+    protected function processCostXParameter(string $value): void
+    {
+        $this->statement->where('free_cost < 0');
+    }
+
+    protected function processRarityParameter(array $values): void
+    {
+        $where = [];
+        $baseKey = ':rarity';
+        $keyCounter = 1;
+
+        for ($i = 0, $ii = count($values); $i < $ii; $i++) {
+            $key = $baseKey . $keyCounter++;
+            $this->bind[$key] = $values[$i];
+            $where[] = "rarity = {$key}";
+        }
+
+        $this->statement->where($where, 'OR');
+    }
+
+    protected function processRaceParameter(string $value): void
+    {
+        $this->statement->where([
+            'race LIKE :race_beginning',
+            'race = :race_exact',
+            'race LIKE :race_middle',
+            'race LIKE :race_end'
+        ], 'OR');
+
+        $this->bind[':race_beginning'] = "{$value}/%";
+        $this->bind[':race_exact'] = $value;
+        $this->bind[':race_middle'] = "%/{$value}/%";
+        $this->bind[':race_end'] = "%/{$value}";
+    }
+
+    protected function processIllustratorParameter(string $value): void
+    {
+        $this->statement->where('artist_name = :artist');
+        $this->bind[':artist'] = $value;
+    }
+}
---
+++ src/app/Services/Resources/Card/Search/Search.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Services\Resources\Card\Search;
+
+use App\Base\Search\Search as BaseSearch;
+use App\Base\Search\SearchInterface;
+use App\Services\Resources\Card\Search\ParameterProcessorsTrait;
+use App\Services\Resources\Card\Search\PostProcessingTrait;
+use App\Services\Lookup\Lookup;
+
+class Search extends BaseSearch
+{
+    use ParameterProcessorsTrait;
+    use PostProcessingTrait;
+
+    protected $parameterProcessors = [
+        'atk'                => 'processAtkValueParameter',
+        'atk-operator'       => 'processAtkOperatorParameter',
+        'attribute'          => 'processAttributeParameter',
+        'attribute-multi'    => 'processMultiAttributeParameter',
+        'attribute-single'   => 'processSingleAttributeParameter',
+        'attribute-selected' => 'processSelectedAttributeParameter',
+        'back-side'          => 'processBackSideParameter',
+        'cluster'            => 'processClusterParameter',
+        'cost'               => 'processCostParameter',
+        'cost-free'          => 'processFreeCostParameter',
+        'cost-x'             => 'processCostXParameter',
+        'def'                => 'processDefValueParameter',
+        'def-operator'       => 'processDefOperatorParameter',
+        'divinity'           => 'processDivinityParameter',
+        'partial-match'      => 'processPartialMatchParameter',
+        'exclude'            => 'processExcludeParameter',
+        'format'             => 'processFormatParameter',
+        'illust'             => 'processIllustratorParameter',
+        'in-fields'          => 'processInFieldsParameter',
+        'q'                  => 'processQueryParameter',
+        'race'               => 'processRaceParameter',
+        'rarity'             => 'processRarityParameter',
+        'set'                => 'processSetParameter',
+        'sort'               => 'processSortParameter',
+        'sort-dir'           => 'processSortDirectionParameter',
+        'type'               => 'processTypeParameter',
+        'type-selected'      => 'processTypeSelectedParameter',
+    ];
+
+    protected $parameterAliases = [
+        'artist'             => 'illust',
+        'attribute-multi'    => 'attribute-multi',
+        'attribute_multi'    => 'attribute-multi',
+        'attribute_selected' => 'attribute-selected',
+        'attributes'         => 'attributes',
+        'attrmulti'          => 'attribute-multi',
+        'attrselected'       => 'attribute-selected',
+        'backside'           => 'back-side',
+        'illustrator'        => 'illust',
+        'infields'           => 'in-fields',
+        'no_attribute_multi' => 'attribute-single',
+        'setcode'            => 'set',
+        'sortdir'            => 'sort-dir',
+        'total_cost'         => 'cost',
+        'type_selected'      => 'type-selected',
+        'xcost'              => 'cost-x',
+    ];
+
+    protected $lookup;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->lookup = Lookup::getInstance();
+    }
+
+    /**
+     * Defines an abstract method
+     *
+     * @return void
+     */
+    protected function setDefaults(): SearchInterface
+    {
+        $this->statement->from('cards');
+
+        $this->statement->fields([
+            'id',
+            'sets_id',
+            'num',
+            'code',
+            'name',
+            'thumb_path',
+            'image_path',
+        ]);
+
+        return $this;
+    }
+}
