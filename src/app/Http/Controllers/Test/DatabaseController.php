12d5d1a6130316b9bd7f74fa8bac8eafb7f99e3b
Alain D'Ettorre <alain.det@gmail.com>
2019-05-07 15:48:39 +0200

    Add test controllers for database and ORM

---
+++ src/app/Http/Controllers/Test/DatabaseController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Base\Controller;
+use App\Http\Request\Request;
+use App\Views\Page;
+
+use App\Services\Database\Statement\SelectSqlStatement;
+use App\Services\Database\Paginator;
+use App\Entity\Card\Card;
+
+class DatabaseController extends Controller
+{
+    public function pagination(Request $request): string
+    {
+        $statement = (new SelectSqlStatement)
+            ->select([
+                "id",
+                "code",
+                "name"
+            ])
+            ->from("cards")
+            ->where("clusters_id = :cluster")
+            ->orderBy("id", "asc")
+            ->setBoundValues([":cluster" => "1"]);
+
+        $paginator = (new Paginator)
+            ->setStatement($statement)
+            ->setPage($request->inputObject()->get("page") ?? 1)
+            ->setResultsPerPage(10)
+            ->setLink($request->getCurrentUrl())
+            ->fetch(Card::class);
+        
+        $results = $paginator->getResults(); // array|ItemsCollection
+        $pagination = $paginator->getPaginationData();
+
+        $resultsHtml = "<ul>";
+        $resultsHtml .= $results->reduce(function ($log, $item) {
+            return $log .= "<li>{$item->code}</li>";
+        }, "");
+        $resultsHtml .= "</ul>";
+
+        $paginationHtml = "<ul>";
+        foreach ($pagination as $name => $value) {
+            $paginationHtml .= "<li><strong>{$name}</strong>: $value</li>";
+        }
+        $paginationHtml .= "</ul>";
+
+        return (
+            "<h1>Results</h1>".
+            "<ul>{$resultsHtml}</ul>".
+            "<h2>Pagination data</h2>".
+            "<ul>{$paginationHtml}</ul>".
+            "<h2>Query</h2>".
+            "<pre>".$statement->toString()."</pre>"
+        );
+    }
+}
---
+++ src/app/Http/Controllers/Test/OrmController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Base\Controller;
+use App\Http\Request\Request;
+use App\Views\Page;
+
+use App\Base\ORM\Manager\EntityManager;
+use App\Entity\Card\Card;
+use App\Entity\GameRuling\GameRuling;
+use App\Entity\GameCluster\GameCluster;
+use App\Entity\GameFormat\GameFormat;
+use App\Entity\PlayRestriction\PlayRestriction;
+
+class OrmController extends Controller
+{
+    public function relatedOneToMany(Request $request): string
+    {
+        // Has game rulings
+        // $cardId = 1046;
+        // $target = GameRuling::class;
+        // $reducer = function ($entity) {
+        //     return $entity->date." - ".substr($entity->text, 0, 10)."...";
+        // };
+
+        // Has play restrictions
+        $cardId = 2735;
+        $target = PlayRestriction::class;
+        $reducer = function ($entity) {
+            return implode(", ", [
+                "format: ".$entity->formats_id,
+                "deck: ".$entity->deck,
+                "copies: ".$entity->copies,
+            ]);
+        };
+
+        $repo = EntityManager::getRepository(Card::class);
+        $card = $repo->findById($cardId);
+        $relatedEntities = $repo->getRelated($card, $target);
+
+        return (
+            "<h1>{$card->name} ({$card->code})</h1>".
+            "<h2>Related: {$target}</h2>".
+            "<ul>".
+                $relatedEntities->reduce(
+                    function ($log, $entity) use (&$reducer) {
+                        return $log .= "<li>".$reducer($entity)."</li>";
+                    }, ""
+                ).
+            "</ul>"
+        );
+    }
+
+    public function relatedManyToOne(Request $request): string
+    {
+        // Has 1 game cluster
+        $cardId = 1;
+        $target = GameCluster::class;
+
+        $repo = EntityManager::getRepository(Card::class);
+        $card = $repo->findById($cardId);
+        $relatedEntity = $repo->getRelated($card, $target);
+        $vars = get_object_vars($relatedEntity);
+
+        $varsLog = "<ul>";
+        foreach ($vars as $name => $value) {
+            $varsLog .= "<li><strong>{$name}: </strong>{$value}</li>";
+        }
+        $varsLog .= "</ul>";
+
+        return (
+            "<h1>{$card->name} ({$card->code})</h1>".
+            "<h2>Related: {$target}</h2>".
+            $varsLog
+        );
+    }
+
+    public function relatedManyToMany(Request $request): string
+    {
+        $sourceId = 1;
+        $target = GameFormat::class;
+
+        $repo = EntityManager::getRepository(GameCluster::class);
+        $source = $repo->findById($sourceId);
+        $targetEntities = $repo->getRelated($source, $target);
+
+        $targetEntitiesLog = "<ul>";
+        foreach ($targetEntities as $entity) {
+            $targetEntitiesLog .= "<li>{$entity->name} ({$entity->code})</li>";
+        }
+        $targetEntitiesLog .= "</ul>";
+
+        return (
+            "<h1>{$source->name} ({$source->code})</h1>".
+            "<h2>Related: {$target}</h2>".
+            $targetEntitiesLog
+        );
+    }
+}
