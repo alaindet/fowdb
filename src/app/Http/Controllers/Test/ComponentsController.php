db93b95722dfb2025fe0586a6c3c3244b94dc7cf
Alain D'Ettorre <alain.det@gmail.com>
2019-05-15 18:41:09 +0200

    Move test templates for components

--- src/app/Http/Controllers/Test/ComponentsController.php
+++ src/app/Http/Controllers/Test/ComponentsController.php
@@ -11,7 +11,7 @@ class ComponentsController extends Controller
     public function buttonCheckbox(Request $request): string
     {
         return (new Page)
-            ->template("test/button-checkbox")
+            ->template("test/components/button-checkbox")
             ->title("Test: button-checkbox component")
             ->minify(false)
             ->render();
@@ -20,7 +20,7 @@ class ComponentsController extends Controller
     public function buttonCheckboxes(Request $request): string
     {
         return (new Page)
-            ->template("test/button-checkboxes")
+            ->template("test/components/button-checkboxes")
             ->title("Test: button-checkboxes component")
             ->minify(false)
             ->render();
@@ -29,9 +29,9 @@ class ComponentsController extends Controller
     public function buttonDropdown(Request $request): string
     {
         return (new Page)
-            ->template("test/button-dropdown")
+            ->template("test/components/button-dropdown")
             ->title("Test: button-dropdown component")
-            ->options(["scripts" => ["test/button-dropdown"]])
+            ->options(["scripts" => ["test/components/button-dropdown"]])
             ->minify(false)
             ->render();
     }
@@ -39,7 +39,7 @@ class ComponentsController extends Controller
     public function buttonRadio(Request $request): string
     {
         return (new Page)
-            ->template("test/button-radio")
+            ->template("test/components/button-radio")
             ->title("Test: button-radio component")
             ->minify(false)
             ->render();
@@ -48,9 +48,9 @@ class ComponentsController extends Controller
     public function inputDropdown(Request $request): string
     {
         return (new Page)
-            ->template("test/input-dropdown")
+            ->template("test/components/input-dropdown")
             ->title("Test: input-dropdown component")
-            ->options(["scripts" => ["test/input-dropdown"]])
+            ->options(["scripts" => ["test/components/input-dropdown"]])
             ->minify(false)
             ->render();
     }
@@ -58,9 +58,9 @@ class ComponentsController extends Controller
     public function selectMultiple(Request $request): string
     {
         return (new Page)
-            ->template("test/select-multiple")
+            ->template("test/components/select-multiple")
             ->title("Test: select-multiple component")
-            ->options(["scripts" => ["test/select-multiple"]])
+            ->options(["scripts" => ["test/components/select-multiple"]])
             ->minify(false)
             ->render();
     }
@@ -76,13 +76,13 @@ class ComponentsController extends Controller
             "lastPage" => 126,
             "lowerBound" => 2476,
             "upperBound" => 2500,
-            "link" => fd_url("test/components/pagination"),
+            "link" => fd_url("test/components/components/pagination"),
             "hasMorePages" => 1,
             "hasAnyPagination" => 1,
         ];
 
         return (new Page)
-            ->template("test/pagination")
+            ->template("test/components/pagination")
             ->title("Test: pagination component")
             ->variables([
                 "pagination" => $pagination,
---
+++ src/frontend/templates/test/render/render-with-vars.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+// VARIABLES
+// $a
+// $b
+// $c
+
+?>
+<h1>Render template</h1>
+<h2>With declared variables</h2>
+
+<ul>
+  <li>a: <?=$a?></li>
+  <li>b: <?=$b?></li>
+  <li>c: <?=$c?></li>
+</ul>
