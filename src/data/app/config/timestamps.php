f354aed4e7c61adac940a725a2a6e30c8ed95429
Alain D'Ettorre <alain.det@gmail.com>
2019-11-08 00:44:13 +0100

    Move asset timestamps to own file

---
+++ src/data/app/config/timestamps.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    "asset.timestamp.generic" => "20191107-2",
+    "asset.timestamp.css" => "20191107-2",
+    "asset.timestamp.js" => "20191107-2",
+    "asset.timestamp.img" => "20191107-2",
+
+];
