0f4f07078b5d5b4599818df3ba4323dc9f95089f
Alain D'Ettorre <alain.det@gmail.com>
2019-11-24 14:45:56 +0100

    Fix missing CR (temporary)

--- src/resources/views/pages/public/cr/index.tpl.php
+++ src/resources/views/pages/public/cr/index.tpl.php
@@ -17,6 +17,16 @@
 </div>
 
 <div class="list-group fd-list-group">
+
+  <div class="list-group-item">
+    <p class="list-group-item-text font-110">
+      2019-11-15
+      <a href="<?=url('documents/cr_9.0.pdf')?>" target="_self">
+        Comprehensive Rules ver. 9.0
+      </a>
+    </p>
+  </div>
+
 	<?php foreach ($items as $item): ?>
   	<div class="list-group-item">
 			<p class="list-group-item-text font-110">
