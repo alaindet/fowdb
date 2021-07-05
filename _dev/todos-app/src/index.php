4b81dc062c2e65998bcbcef32029b8abbbbee04a
Alain D'Ettorre <alain.det@gmail.com>
2021-07-05 18:46:08 +0200

    WIP: Add nikix/fastroute

--- _dev/todos-app/src/index.php
+++ _dev/todos-app/src/index.php
@@ -13,37 +13,81 @@ $request = new Request();
 $request->computeBody();
 $response = null;
 
-switch ($_GET['uri']) {
+$dispatcher = \FastRoute\simpleDispatcher(
+    function(\FastRoute\RouteCollector $r) {
+        $r->addRoute('GET', '/', 'get_home');
+        $r->addRoute('GET', '/users', 'get_all_users_handler');
+        $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+        $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+    }
+);
 
-    case 'todos/create':
-        $controller = new \App\Features\Todos\Controllers\TodosController($config);
-        $body = $request->getBody();
-        $response = $controller->create($body);
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+\App\Shared\Utils\Utils::dump('req data', [
+    'httpMethod' => $httpMethod,
+    'uri' => $uri,
+]);
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
         break;
-
-    case 'todos/all':
-        $controller = new \App\Features\Todos\Controllers\TodosController($config);
-        $response = $controller->getAll();
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
         break;
-
-    case 'todos/single':
-        $controller = new \App\Features\Todos\Controllers\TodosController($config);
-        $id = $_GET['id'];
-        $response = $controller->getOne($id);
-        break;
-
-    case 'todos/update':
-        $controller = new \App\Features\Todos\Controllers\TodosController($config);
-        $id = $_GET['id'];
-        $body = $request->getBody();
-        $response = $controller->update($id, $body);
-        break;
-
-    case 'todos/delete':
-        $controller = new \App\Features\Todos\Controllers\TodosController($config);
-        $id = $_GET['id'];
-        $response = $controller->delete($id);
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
         break;
 }
 
-echo $response;
+\App\Shared\Utils\Utils::dump('routeInfo', $routeInfo);
+
+echo "HELLO WORLD";
+
+// switch ($_GET['uri']) {
+
+//     case 'todos/create':
+//         $controller = new \App\Features\Todos\Controllers\TodosController($config);
+//         $body = $request->getBody();
+//         $response = $controller->create($body);
+//         break;
+
+//     case 'todos/all':
+//         $controller = new \App\Features\Todos\Controllers\TodosController($config);
+//         $response = $controller->getAll();
+//         break;
+
+//     case 'todos/single':
+//         $controller = new \App\Features\Todos\Controllers\TodosController($config);
+//         $id = $_GET['id'];
+//         $response = $controller->getOne($id);
+//         break;
+
+//     case 'todos/update':
+//         $controller = new \App\Features\Todos\Controllers\TodosController($config);
+//         $id = $_GET['id'];
+//         $body = $request->getBody();
+//         $response = $controller->update($id, $body);
+//         break;
+
+//     case 'todos/delete':
+//         $controller = new \App\Features\Todos\Controllers\TodosController($config);
+//         $id = $_GET['id'];
+//         $response = $controller->delete($id);
+//         break;
+// }
+
+// echo $response;
